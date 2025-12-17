768e73400eca3356c211c7093865460c836ff12e
Asya-sun <asyasunsea@gmail.com>
2025-12-18 06:03:27 +0700

    deadlock detector added

--- SixthLaba/TableService/Program.cs
+++ SixthLaba/TableService/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddSwaggerGen();
 // Ваши сервисы
 builder.Services.AddSingleton<ITableManager, TableManagerService>();
 builder.Services.AddSingleton<IMetricsCollector, MetricsCollectorService>();
+builder.Services.AddHostedService<DeadlockDetector>();
 
 // Настройка CORS (для локальной разработки)
 builder.Services.AddCors(options =>
